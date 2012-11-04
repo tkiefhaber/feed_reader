7e95d2784cd47a71e19a2dbae258d9db1a2a549c
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-11-03 20:36:41 -0400

    adding more feeds

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -10,9 +10,15 @@ class FeedEntry < ActiveRecord::Base
     wwtdd = "http://www.wwtdd.com/feed"
     uncrate = "http://feeds.feedburner.com/uncrate"
     devour = "http://feeds.feedburner.com/devourfeed"
+    rusty_chicken = "http://rustychicken.tumblr.com/rss"
+    help_help = "http://helphelp.tumblr.com/rss"
+    mr_crothers = "http://mrcrothers.tumblr.com/rss"
+    the_chau_show = "http://thechaushow.tumblr.com/rss"
+    i_want_to_steak = "http://iwanttosteak.tumblr.com/rss"
+
     loop do
       sleep 3.minutes
-      feed_urls = [people_news, wwtdd, uncrate, devour]
+      feed_urls = [people_news, wwtdd, uncrate, devour, help_help, mr_crothers, the_chau_show, i_want_to_steak]
       feed_urls.each do |feed_url|
         feed = Feedzirra::Feed.fetch_and_parse(feed_url)
         add_entries(feed.entries)
